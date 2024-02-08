c8fb0ae8da75827771e3573de96cf723c60d0dd1
Bananakluay <150219234+Bananakluay@users.noreply.github.com>
2024-02-08 20:39:01 +0700

    Create GameStatesManager.java

---
+++ TrashGame/src/gameState/GameStatesManager.java
@@ -0,0 +1,29 @@
+package gameState;
+@SuppressWarnings("unused")
+public class GameStatesManager {
+
+    private MenuState menu;
+    private PlayingState playing;
+    private PausedState paused;
+
+    private GameState currenState;
+
+    public GameStatesManager(){
+        playing = new PlayingState();
+        setState(playing);
+
+        
+    }
+
+    public void setState(GameState state){
+        currenState = state;
+    }
+    public GameState getCurrentState(){
+        return currenState;
+    }
+
+    public GameStatesManager getGameStatesManager(){
+        return this;
+    }
+
+}
