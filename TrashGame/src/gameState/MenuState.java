900a092d0f893488f9abf750175a7a5f4e4ff37f
Bananakluay <150219234+Bananakluay@users.noreply.github.com>
2024-02-08 20:39:33 +0700

    Create MenuState.java

---
+++ TrashGame/src/gameState/MenuState.java
@@ -0,0 +1,17 @@
+package gameState;
+
+import java.awt.Graphics;
+
+public class MenuState extends GameState{
+    @Override
+    public void draw(Graphics g) {
+        //asdfasdfasdfasdfasfasfa
+        //asdfasdfasdfasdfasdfasdf
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+    
+}
