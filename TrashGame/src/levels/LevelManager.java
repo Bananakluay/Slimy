b45c4b55ec336fbfb72aac925cdfef98a0c4eb7c
Bananakluay <150219234+Bananakluay@users.noreply.github.com>
2024-02-08 20:39:04 +0700

    Create LevelManager.java

---
+++ TrashGame/src/levels/LevelManager.java
@@ -0,0 +1,26 @@
+package levels;
+
+import java.awt.Graphics;
+
+import gameData.Data;
+import inputImage.Sprite;
+
+public class LevelManager {
+    private Level level;
+    public LevelManager(){
+        level = new Level(Sprite.loadSprite(Data.TileData.TEST_MAP));
+    }
+
+    public void update(){
+        level.update();
+    }
+
+    public void draw(Graphics g){
+        level.draw(g);
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+}
