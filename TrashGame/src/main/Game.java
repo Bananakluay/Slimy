351b079cec6319798056ddf326e1bce646c02cae
Bananakluay <150219234+Bananakluay@users.noreply.github.com>
2024-02-08 20:39:05 +0700

    Create Game.java

---
+++ TrashGame/src/main/Game.java
@@ -0,0 +1,104 @@
+package main;
+
+import java.awt.Graphics;
+import gameState.GameStatesManager;
+
+@SuppressWarnings("unused")
+public class Game implements Runnable{
+
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
+
+    public final static int TILES_DEFAULT_SIZE = 16;
+    public final static float SCALE = 4f;
+    public final static int TILES_IN_WIDTH = 26;
+    public final static int TILES_IN_HEIGHT = 14;
+    public final static int TILES_SIZE = (int)(TILES_DEFAULT_SIZE * SCALE);
+    public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
+    public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
+
+	private GameStatesManager gsm;
+	
+    private GameWindow gameWindow;
+    private GamePanel gamePanel;
+
+
+    private Thread gameThread;
+
+    public Game(){
+		gsm = new GameStatesManager();
+        gamePanel = new GamePanel(this);
+        gameWindow = new GameWindow(gamePanel);
+        gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+    public void ready(){
+
+    }
+    public void update(){
+		gsm.getCurrentState().update();
+    }
+
+    public void render(Graphics g){
+		//playing 
+		//
+		gsm.getCurrentState().draw(g);
+    }
+    
+    private void startGameLoop() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+
+		double timePerFrame = 1000000000.0 / FPS_SET;
+		double timePerUpdate = 1000000000.0 / UPS_SET;
+
+		long previousTime = System.nanoTime();
+
+		int frames = 0;
+		int ticks = 0;
+		long lastCheck = System.currentTimeMillis();
+
+		double deltaU = 0;
+		double deltaF = 0;
+
+		while (true) {
+			long currentTime = System.nanoTime();
+
+			deltaU += (currentTime - previousTime) / timePerUpdate;
+			deltaF += (currentTime - previousTime) / timePerFrame;
+			previousTime = currentTime;
+
+			if (deltaU >= 1) {
+				update();
+				ticks++;
+				deltaU--;
+			}
+
+			if (deltaF >= 1) {
+				gamePanel.repaint();
+				frames++;
+				deltaF--;
+			}
+
+			if (System.currentTimeMillis() - lastCheck >= 1000) {
+				lastCheck = System.currentTimeMillis();
+				// System.out.println("FPS: " + frames + " | UPS: " + ticks);
+				frames = 0;
+				ticks = 0;
+
+			}
+		}
+
+	}
+
+	public GameStatesManager getGameStatesManager(){
+		return gsm;
+	}
+
+
+}
