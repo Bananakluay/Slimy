81539ca1be459bee32d57681cc6b3da0f957984c
tin-auppati <tin_auppati@cmu.ac.th>
2024-02-11 22:43:02 +0700

    switch walk

--- TrashGame/src/entities/Player.java
+++ TrashGame/src/entities/Player.java
@@ -9,120 +9,124 @@ import input.Controller;
 import main.Game;
 import simplePhysics.Area;
 import simplePhysics.RigidBody;
+
 @SuppressWarnings("unused")
-public class Player extends RigidBody implements Controller{
+public class Player extends RigidBody implements Controller {
 
     // protected float _acc = 0.25f*Game.SCALE, _dcc = 0.25f*Game.SCALE;
-    private boolean Left, Right, Up, Down;
+    protected boolean Left, Right, Up, Down;
 
-    private float minVelX = -0.75f*Game.SCALE;
-    private float maxVelX = 0.75f*Game.SCALE;
+    private float minVelX = -0.75f * Game.SCALE;
+    private float maxVelX = 0.75f * Game.SCALE;
 
-    private float jumpForce = 1.5f*Game.SCALE;
-    private float downForce = 1.6f*Game.SCALE;
+    private float jumpForce = 1.5f * Game.SCALE;
+    private float downForce = 1.6f * Game.SCALE;
 
     private boolean isCurrentPlayer;
 
-
     private Color colorPlayer = Color.white; // for debug
+
     public Player(Area hitbox) {
         super(hitbox);
     }
 
     @Override
-    public void update(){
-        //velx = 0
-        if(isCurrentPlayer) 
+    public void update() {
+        // velx = 0
+        if (isCurrentPlayer)
             updateVelocity();
-        //velX = -12
+        // velX = -12
         super.update();
     }
 
-    public void draw(Graphics g){
-        Graphics2D g2d = (Graphics2D)g;
-        
+    public void draw(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
         // g2d.draw((int)hitbox.x, (int)hitbox.y, (int)100, (int)100);
-        
+
         g2d.setColor(colorPlayer);
         g2d.fill(this.hitbox);
 
         // g2d.setColor(Color.red);
         // g2d.draw(getBoundsX());
-        
+
         // g2d.setColor(Color.blue);
         // g2d.draw(getBoundsY());
-        
+
         g2d.setColor(Color.red);
         g2d.draw(getFloorHitbox());
 
     }
 
-    private void updateVelocity(){  
-        
+    private void updateVelocity() {
+
         movement();
         jumping();
 
         velX = clamp(velX, minVelX, maxVelX);
         velY = clamp(velY, -jumpForce, downForce);
     }
-    
-    private void movement(){
-        if(Left && Right || !Left && !Right)
-            velX*= 0.5;
-        else if(Left && !Right)
+
+    private void movement() {
+        if (Left && Right || !Left && !Right)
+            velX *= 0.5;
+        else if (Left && !Right)
             velX--;
-        else if(Right && !Left)
+        else if (Right && !Left)
             velX++;
 
-        if(velX > 0 && velX < 0.75)
+        if (velX > 0 && velX < 0.75)
             velX = 0;
-        if(velX > -0.75 && velX < 0)    
+        if (velX > -0.75 && velX < 0)
             velX = 0;
     }
 
-    private void jumping(){
-        if(Up && isOnFloor)
+    private void jumping() {
+        if (Up && isOnFloor)
             velY -= jumpForce;
     }
 
-    public void setColor(Color color){
+    public void setColor(Color color) {
         colorPlayer = color;
     }
 
-    public void reset(){
+    public void reset() {
         Left = false;
         Right = false;
         Up = false;
-        Down =false;
+        Down = false;
         isCurrentPlayer = false;
     }
-    public void setIsCurrentPlayer(){
+
+    public void setIsCurrentPlayer() {
         isCurrentPlayer = true;
     }
+
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
             case KeyEvent.VK_W:
-                Up = true; 
+                Up = true;
                 Down = false; // for smooth switch direction
                 break;
-            case KeyEvent.VK_LEFT: 
+            case KeyEvent.VK_LEFT:
             case KeyEvent.VK_A:
-                Left = true; 
+                Left = true;
                 Right = false; // for smooth switch direction
                 break;
             case KeyEvent.VK_DOWN:
             case KeyEvent.VK_S:
-                Down = true; 
-                Up =false; // for smooth switch direction
+                Down = true;
+                Up = false; // for smooth switch direction
                 break;
             case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_D:
-                Right = true; 
+                Right = true;
                 Left = false;
-                break;            
-            default: break;
+                break;
+            default:
+                break;
         }
     }
 
@@ -131,18 +135,23 @@ public class Player extends RigidBody implements Controller{
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
             case KeyEvent.VK_W:
-                Up = false; break;
-            case KeyEvent.VK_LEFT: 
+                Up = false;
+                break;
+            case KeyEvent.VK_LEFT:
             case KeyEvent.VK_A:
-                Left = false; break;
+                Left = false;
+                break;
             case KeyEvent.VK_DOWN:
             case KeyEvent.VK_S:
-                Down = false; break;
+                Down = false;
+                break;
             case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_D:
-                Right = false; break;
-            default: break;
+                Right = false;
+                break;
+            default:
+                break;
         }
     }
-    
+
 }
--- TrashGame/src/entities/PlayerManager.java
+++ TrashGame/src/entities/PlayerManager.java
@@ -3,7 +3,6 @@ package entities;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
-
 import main.Game;
 import simplePhysics.Area;
 
@@ -11,53 +10,67 @@ public class PlayerManager {
     Player greenSlime;
     Player yellowSlime;
     Player currentPlayer;
+    Player lastPlayer;
 
-    public PlayerManager(){
+    public PlayerManager() {
         setup();
     }
 
-    public void setup(){
-        greenSlime = new Player(new Area(5*Game.TILES_SIZE, 5*Game.TILES_SIZE, Game.TILES_SIZE, Game.TILES_SIZE));
+    public void setup() {
+        greenSlime = new Player(new Area(5 * Game.TILES_SIZE, 5 * Game.TILES_SIZE, Game.TILES_SIZE, Game.TILES_SIZE));
         greenSlime.setColor(Color.green);
 
-        yellowSlime = new Player(new Area(15*Game.TILES_SIZE, 5*Game.TILES_SIZE, Game.TILES_SIZE, Game.TILES_SIZE));
+        yellowSlime = new Player(new Area(15 * Game.TILES_SIZE, 5 * Game.TILES_SIZE, Game.TILES_SIZE, Game.TILES_SIZE));
         yellowSlime.setColor(Color.yellow);
 
         currentPlayer = greenSlime;
         currentPlayer.setIsCurrentPlayer();
-        
+
     }
 
-    public void update(){
+    public void update() {
         greenSlime.update();
         yellowSlime.update();
     }
 
-    public void draw(Graphics g){
+    public void draw(Graphics g) {
         greenSlime.draw(g);
         yellowSlime.draw(g);
     }
-    
-    public void switchPlayer(){
-        if(currentPlayer == greenSlime){
+
+    public void switchPlayer() {
+        if (currentPlayer == greenSlime) {
             currentPlayer = yellowSlime;
             currentPlayer.setIsCurrentPlayer();
+            lastPlayer = greenSlime;
+            simulateMovementKey();
             greenSlime.reset();
-            
-        }
-        else if(currentPlayer == yellowSlime){
+
+        } else if (currentPlayer == yellowSlime) {
             currentPlayer = greenSlime;
             currentPlayer.setIsCurrentPlayer();
-            yellowSlime.reset();      
+            lastPlayer = yellowSlime;
+            simulateMovementKey();
+            yellowSlime.reset();
         }
 
     }
-    public void keyPressed(KeyEvent e){
-        if(e.getKeyCode() == KeyEvent.VK_R)
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_R)
             switchPlayer();
         currentPlayer.keyPressed(e);
     };
-	public void keyReleased(KeyEvent e){
+
+    public void keyReleased(KeyEvent e) {
         currentPlayer.keyReleased(e);
     };
+
+    private void simulateMovementKey() {
+        if (lastPlayer.Left) {
+            currentPlayer.Left = true;
+        } else if (lastPlayer.Right) {
+            currentPlayer.Right = true;
+        }
+    }
 }
