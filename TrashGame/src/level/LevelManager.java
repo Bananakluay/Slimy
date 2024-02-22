a72529a1f60091ef95834501332cad6660f92478
cinnamonzan <tanyalaklaotrakool@gmail.com>
2024-02-23 00:58:36 +0700

    map 3

--- TrashGame/src/level/LevelManager.java
+++ TrashGame/src/level/LevelManager.java
@@ -20,7 +20,7 @@ public class LevelManager {
 
     private void init(){
         //buildAllLevels();
-        currentLevel = new Level("TrashGame/res/t2.png",levelScene);
+        currentLevel = new Level("TrashGame/res/t3.png",levelScene);
     }
 
     public static LevelManager get(LevelScene levelScene){
