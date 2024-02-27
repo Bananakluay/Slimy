ded628b5ba15f4d24aab7fd968144fb414dd5a1f
Bananakluay <150219234+Bananakluay@users.noreply.github.com>
2024-02-28 01:23:20 +0700

    Update EntityManager.java

--- TrashGame/src/entity/EntityManager.java
+++ TrashGame/src/entity/EntityManager.java
@@ -4,74 +4,92 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import Scene.LevelScene;
 import components.Component;
+import scene.LevelScene;
 
 public class EntityManager {
 
     private List<Entity> entities;
+    List<Entity> entitiesToRemove;
+    List<Entity> entitiesToAdd;
 
-    public EntityManager(){
+    public EntityManager() {
         entities = new ArrayList<>();
+        entitiesToAdd = new ArrayList<>();
+        entitiesToRemove = new ArrayList<>();
     }
 
     public void updateEntities() {
         Iterator<Entity> iterator = entities.iterator();
+
+        for (Entity entity : entitiesToAdd) {
+            entities.add(entity);
+            LevelScene.getRenderer().submit(entity);
+        }
+        entitiesToAdd.clear();
         while (iterator.hasNext()) {
             Entity entity = iterator.next();
             entity.update();
         }
+
+        for (Entity entity : entitiesToRemove) {
+            entities.remove(entity);
+            LevelScene.getRenderer().remove(entity, entity.getZindex());
+        }
+        entitiesToRemove.clear();
     }
-    public void ready(){
-		for(Entity entity : entities){
-			entity.ready();
-		}
-	}
-    public void addEntity(Entity entity){
+
+    public void ready() {
+        for (Entity entity : entities) {
+            entity.ready();
+        }
+    }
+
+    public void addEntity(Entity entity) {
         entity.ready();
         entities.add(entity);
         LevelScene.getRenderer().submit(entity);
 
     }
 
-    public void addAllEntities(List<Entity> entities){
+    public void addAllEntities(List<Entity> entities) {
         this.entities = entities;
     }
-    
-
 
     public void removeEntity(Entity entity) {
-        System.out.println("remove : " + entity.getName());
         entities.remove(entity);
-        LevelScene.getRenderer().remove(entity,entity.getZindex()); 
+        LevelScene.getRenderer().remove(entity, entity.getZindex());
     }
 
+    public Entity getEntity(String name) {
+        for (Entity entity : this.entities) {
+            if (entity.getName().equals(name))
+                return entity;
+        }
+        return null;
+    }
 
-	public Entity getEntity(String name){
-		for(Entity entity : this.entities){
-			if(entity.getName().equals(name))
-				return entity;
-		}
-		return null;
-	}
+    public Entity getEntity(int id) {
+        for (Entity entity : this.entities) {
+            if (entity.getId() == id)
+                return entity;
+        }
+        return null;
+    }
 
-    public Entity getEntity(int id){
-		for(Entity entity : this.entities){
-			if(entity.getId() == id)
-				return entity;
-		}
-		return null;
-	}
+    public List<Entity> getEntitiesWithComponent(Class<? extends Component> componentClass) {
+        List<Entity> res = new ArrayList<>();
+        for (Entity entity : entities)
+            if (entity.hasComponent(componentClass))
+                res.add(entity);
+        return res;
+    }
 
-	public List<Entity> getEntitiesWithComponent(Class<? extends Component> componentClass){
-		List<Entity> res = new ArrayList<>();
-		for(Entity entity : entities)
-			if(entity.hasComponent(componentClass))
-				res.add(entity);
-		return res;
-	}
-
-	public List<Entity> getAllEntities(){return entities;}
-
+    public void clear() {
+        entities.clear();
+    } 
+    public List<Entity> getAllEntities() {
+        return entities;
+    }
 
 }
