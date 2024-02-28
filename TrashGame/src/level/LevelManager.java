9503407de0ce89193732838291339344533651df
Bananakluay <150219234+Bananakluay@users.noreply.github.com>
2024-02-28 23:06:12 +0700

    Update LevelManager.java

--- TrashGame/src/level/LevelManager.java
+++ TrashGame/src/level/LevelManager.java
@@ -22,29 +22,32 @@ public class LevelManager {
     private static Timer timer;
 
     public static boolean isGameOver = false;
+
     private LevelManager() {
         init();
     }
-    
+
     public static LevelManager get() {
         if (levelManager == null)
             levelManager = new LevelManager();
         return levelManager;
     }
+
     private void init() {
         loadLevels();
     }
 
     public static void loadLevels() {
         buildAllLevels();
-        currentLevel = new Level("TrashGame/res/lvlstest/" + Map.get(lvlindex));
+        currentLevel = new Level("TrashGame/res/lvls/" + Map.get(lvlindex));
     }
+
     public Level getCurrentLevel() {
         return currentLevel;
     }
 
     public static File[] GetAllLevels() {
-        URL url = LevelManager.class.getResource("/lvlstest");
+        URL url = LevelManager.class.getResource("/lvls");
         File file = null;
 
         try {
@@ -70,14 +73,14 @@ public class LevelManager {
         if (lvlindex < Map.size()) {
             // LevelScene.deleteCurrentLevel();
             LevelScene.createNextLevel();
-            currentLevel = new Level("TrashGame/res/lvlstest/" + Map.get(lvlindex));
+            currentLevel = new Level("TrashGame/res/lvls/" + Map.get(lvlindex));
             lvlindex++;
         } else {
             System.out.println("Game complete");
         }
     }
 
-    public static void resetLevel(){
+    public static void resetLevel() {
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
@@ -88,7 +91,7 @@ public class LevelManager {
                 timer.cancel();
             }
         }, 2000);
-        
+
     }
 
     public static void onDestroy() {
