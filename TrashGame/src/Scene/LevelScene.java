5669c1177bb668675725a362f5e7c907f0f54521
Wutthipat T <wutthipat_t@cmu.ac.th>
2024-02-28 21:03:09 +0700

    menu

--- TrashGame/src/Scene/LevelScene.java
+++ TrashGame/src/Scene/LevelScene.java
@@ -8,6 +8,7 @@ import static utils.Constants.Game.TILES_SIZE;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import components.Bounds;
 import dataStructure.AssetPool;
@@ -17,6 +18,7 @@ import entity.EntityManager;
 import entity.EntityType;
 import gui.GuiButton;
 import gui.GuiLayer;
+import gui.GuiText;
 import gui.ToggleGuiButton;
 import level.LevelManager;
 import main.Game;
@@ -33,6 +35,7 @@ import utils.Vec2;
 public class LevelScene extends Scene {
 
     private static PlayerManager playerManager;
+    private static BufferedImage pauseBackground;
 
     private static EntityManager entitiyManager;
     private GuiLayer guiPlayingScene;
@@ -54,6 +57,8 @@ public class LevelScene extends Scene {
     public void init() {
         // Entity--------------------------------
         LevelManager.loadLevels();
+
+        pauseBackground = AssetPool.getBufferedImage("TrashGame/res/assets/ui/test_pause_menu.png", 258, 389);
 
         Platform p = new Platform(TILES_SIZE * 10, TILES_SIZE * 10);
         entitiyManager.addEntity(p);
@@ -79,25 +84,41 @@ public class LevelScene extends Scene {
     public void initGuiPauseScene() {
         ToggleGuiButton soundButton = new ToggleGuiButton(
                 "SoundButton",
-                new Vec2(TILES_SIZE * 10, TILES_SIZE * 3),
+                new Vec2((float) (TILES_SIZE * 17.15), (float) (TILES_SIZE * 4.8)),
                 new Vec2(16f * SCALE * 1.5f, 16f * SCALE * 1.5f),
                 AssetPool.getBufferedImageList("TrashGame/res/assets/ui/SoundButton.png", 16, 16),
                 () -> System.out.println("SoundButton"));
 
+        ToggleGuiButton musicButton = new ToggleGuiButton(
+                    "musicButton",
+                    new Vec2((float) (TILES_SIZE * 13.45), (float) (TILES_SIZE * 4.8)),
+                    new Vec2(16f * SCALE * 1.5f, 16f * SCALE * 1.5f),
+                    AssetPool.getBufferedImageList("TrashGame/res/assets/ui/SoundButton.png", 16, 16),
+                    () -> System.out.println("musicButton"));
+
         GuiButton continueButton = new GuiButton( /* gen option button */
                 "ContinueButton",
-                new Vec2(GAME_WIDTH - (40f * SCALE), GAME_HEIGHT * 0.03f), // position
+                new Vec2(GAME_WIDTH / 2 - (16f * SCALE), GAME_HEIGHT * 0.595f), // position
                 new Vec2(16f * SCALE * 2, 16f * SCALE * 2),
                 AssetPool.getBufferedImageList("TrashGame/res/assets/ui/ContinueButton.png", 16, 16), // change png
                 () -> isRunning = true);
 
         GuiButton backButton = new GuiButton( /* gen option button */
                 "backButton",
-                new Vec2(GAME_WIDTH - (50f * SCALE), GAME_HEIGHT * 0.84f), // position
+                new Vec2(GAME_WIDTH / 2 - (75f * SCALE), GAME_HEIGHT * 0.595f), // position
                 new Vec2(16f * SCALE * 2, 16f * SCALE * 2),
                 AssetPool.getBufferedImageList("TrashGame/res/assets/ui/BackToLSMButton.png", 16, 16), // change png
                 () -> SceneManager.changeScene(Scenes.MENU_SCENE));
 
+        GuiButton restartButton = new GuiButton( /* gen option button */
+                "restartButton",
+                new Vec2(GAME_WIDTH / 2 - (-42f * SCALE), GAME_HEIGHT * 0.595f), // position
+                new Vec2(16f * SCALE * 2, 16f * SCALE * 2),
+                AssetPool.getBufferedImageList("TrashGame/res/assets/ui/ResetButton.png", 16, 16), // change png
+                () -> init());
+        
+        guiPauseScene.addGuiComponent(restartButton);
+        guiPauseScene.addGuiComponent(musicButton);
         guiPauseScene.addGuiComponent(backButton);
         guiPauseScene.addGuiComponent(continueButton);
         guiPauseScene.addGuiComponent(soundButton);
@@ -133,7 +154,20 @@ public class LevelScene extends Scene {
             guiPlayingScene.render(g);
         } else {
             renderer.render(g);
+            g.setColor(new Color(0, 0, 0, 128)); 
+            g.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
             guiPauseScene.render(g);
+            g.setColor(Color.BLACK);
+            g.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT); // draw center
+            g.drawLine((GAME_WIDTH / 2) - 175, 0, (GAME_WIDTH / 2) - 175, GAME_HEIGHT); // draw center
+            g.drawLine((GAME_WIDTH / 2) + 175, 0, (GAME_WIDTH / 2) + 175, GAME_HEIGHT); // draw center
+        
+            GuiText.drawString(
+                g,
+                "PAUSED",
+                new Vec2(GAME_WIDTH / 2, (float) (GAME_HEIGHT / 2.2)),
+                Color.BLACK,
+                AssetPool.getFont("TrashGame/res/assets/fonts/m3x6.ttf", 32 * 10));
         }
         renderer.processQueuedChanges();
     }
@@ -168,4 +202,9 @@ public class LevelScene extends Scene {
         return playerManager;
     }
 
+    private void renderPauseBackground(Graphics g) {
+        if (!isRunning && pauseBackground != null) {
+            g.drawImage(pauseBackground, (int) (GAME_WIDTH / 2 - (33f * SCALE)), (int) (GAME_HEIGHT * 0.25f), null); // Render the pause menu background at (0, 0)
+        }
+    }
 }
